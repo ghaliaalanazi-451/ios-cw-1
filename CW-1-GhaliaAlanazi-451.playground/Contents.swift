db8544068c740490c6c5412c652c10582b0d28ea
ghaliaalanazi-451 <66477134+ghaliaalanazi-451@users.noreply.github.com>
2020-06-14 00:44:16 +0300

    تم

---
+++ CW-1-GhaliaAlanazi-451.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+
+let name = " ghalia "
+
+var hobby1 = " cooking "
+var hobby2 = " coding "
+var age = 16
+var height = 1.65
+
+
+print(" my name is \(name) , iam \(age) years old , my height is \(height) , my hobbies are\(hobby1),\(hobby2). ")
+
+
+
+let FriendName = " Waad "
+
+var FriendAge = 16
+
+var FriendHeight = 1.60
+
+var FriendHobby1 = " cooking "
+
+var FriendHobby2 = " reading "
+
+
+//Comparing between me and my friend
+
+var age_difference  = age-FriendAge
+
+name == FriendName
+name != FriendName
+
+hobby1 == FriendHobby1
+hobby1 != FriendHobby1
+hobby2 == FriendHobby2
+hobby2 != FriendHobby2
+hobby1 == FriendHobby2
+hobby1 != FriendHobby2
+hobby2 == FriendHobby1
+hobby2 != FriendHobby1
+
+age > FriendAge
+age < FriendAge
+age <= FriendAge
+age >= FriendAge
+age == FriendAge
+age != FriendAge
+
+
+height != FriendHeight
+
+
+
+
+
+
+
+
+
+
